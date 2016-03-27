b1d896476e2cbf662edec6f56f708708e36bc4a7
Ahn Jungho <ahnjungho@ahnjungho.org>
2016-03-27 20:57:45 +0900

    update deploy script

--- README.md
+++ README.md
@@ -19,3 +19,28 @@ $ python manage.py loaddata ./pyconkr/fixtures/flatpages.json
 $ bower install
 $ python manage.py runserver
 ```
+
+
+## deploy
+
+서버 정보를 담고 있는 deploy/server.py 파일과 fabric이 필요합니다.
+
+server.py 파일 예제.
+
+``` python
+# deploy/server.py
+
+from fabric.api import env
+
+env.pycon_user = 'username'
+env.pycon_host = '123.456.789.012'
+env.pycon_port = '1234'
+```
+
+fabric
+
+``` shell
+$ fab deploy --set=target=dev
+# or
+$ fab deploy --set=target=www
+```
--- fabfile.py
+++ fabfile.py
@@ -2,19 +2,14 @@
 import os
 
 from fabric.api import local, run, cd, prefix, env, sudo
-
-if 'PYCON_HOST' not in os.environ:
-    raise Exception('PYCON_HOST should be exported')
-if 'PYCON_PORT' not in os.environ:
-    raise Exception('PYCON_PORT should be exported')
-if 'PYCON_USER' not in os.environ:
-    raise Exception('PYCON_USER should be exported')
+from deploy import server
 
 env.host_string = '{user}@{host}:{port}'.format(
-                                            user=os.environ['PYCON_USER'],
-                                            host=os.environ['PYCON_HOST'],
-                                            port=os.environ['PYCON_PORT']
+                                            user=env.pycon_user,
+                                            host=env.pycon_host,
+                                            port=env.pycon_port
                                             )
+
 
 def deploy(target='dev', sha1=None):
     if sha1 is None:
@@ -22,9 +17,20 @@ def deploy(target='dev', sha1=None):
         sha1 = local('git rev-parse HEAD', capture=True)
     # server code reset to current working sha1
     home_dir = '/home/pyconkr/{target}.pycon.kr/pyconkr-2016'.format(target=target)
+    python_env = '/home/pyconkr/.pyenv/versions/pyconkr-2016'
     with cd(home_dir):
         sudo('git fetch --all -p', user='pyconkr')
         sudo('git reset --hard ' + sha1, user='pyconkr')
+        sudo('bower install', user='pyconkr')
+        sudo('%s/bin/pip install -r requirements.txt' % python_env, user='pyconkr')
+        sudo('%s/bin/python manage.py compilemessages' % python_env, user='pyconkr')
+        sudo('%s/bin/python manage.py makemigrations' % python_env, user='pyconkr')
+        sudo('%s/bin/python manage.py migrate' % python_env, user='pyconkr')
+        sudo('%s/bin/python manage.py collectstatic --noinput' % python_env, user='pyconkr')
+        sudo('%s/bin/python manage.py loaddata pyconkr/fixtures/flatpages.json' % python_env, user='pyconkr')
     # worker reload
     with cd(home_dir):
-        sudo('./restart.sh', user='pyconkr')
+        if target == 'dev':
+            sudo('restart pyconkr-2016/uwsgi-%s' % target)
+        else:
+            sudo('restart pyconkr-2016/uwsgi' % target)
