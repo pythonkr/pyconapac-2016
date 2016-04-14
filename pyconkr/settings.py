9ec1eb5927b83b27d051c2ef4e00919e59b453b7
woojing <woojing.seok@gmail.com>
2016-04-14 14:38:21 +0900

    add registration receipt page

--- pyconkr/settings.py
+++ pyconkr/settings.py
@@ -162,9 +162,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "pyconkr/static"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "pyconkr/static"),
+# )
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 STATIC_URL = '/static/'
 
